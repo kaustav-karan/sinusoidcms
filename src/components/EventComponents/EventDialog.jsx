96651a186e051ee6e5eb7c4620e2d4e319391eaa
Kaustav <kaustavkaran2015@gmail.com>
2024-10-16 17:22:40 +0530

    Refactor: Update EventDialog UI and add select dropdown for event status

--- src/components/EventComponents/EventDialog.jsx
+++ src/components/EventComponents/EventDialog.jsx
@@ -76,7 +76,6 @@ export default function EventDialog({ open, onClose, event, newEvent }) {
     window.location.reload();
   }
 
-
   function onChangeText(e, key) {
     setEventData((prev) => ({
       ...prev,
@@ -165,158 +164,196 @@ export default function EventDialog({ open, onClose, event, newEvent }) {
             disabled={loading}
           />
 
-          {/* <TextField
-            id={`${event?.status}-dialog`}
-            label="Status"
-            value={eventData?.status}
-            onChange={(e) => onChangeText(e, "status")}
-            disabled={loading}
-          /> */}
-
-          <FormControl>
-            <InputLabel id={`${event?.status}-dialog`}>Status</InputLabel>
-            <Select
-              label="Status"
-              id={`${event?.status}-dialog-input`}
-              value={eventData?.status}
-              onChange={(e) => onChangeText(e, "status")}
-              disabled={loading}
-            >
-              <MenuItem value="upcoming">Upcoming</MenuItem>
-              <MenuItem value="registrations">Registrations</MenuItem>
-              <MenuItem value="ongoing">Ongoing</MenuItem>
-              <MenuItem value="ended">Ended</MenuItem>
-            </Select>
-          </FormControl>
-
-          <TextField
-            id={`${eventData?.eventType}-dialog`}
-            label="Event Type"
-            value={eventData?.eventType}
-            onChange={(e) => onChangeText(e, "eventType")}
-            disabled={loading}
-          />
-
-          <TextField
-            id={`${eventData?.eventParticipants}-dialog`}
-            label="Event Participants"
-            value={eventData?.eventParticipants}
-            onChange={(e) => onChangeText(e, "eventParticipants")}
-            disabled={loading}
-          />
-
-          <TextField
-            id={`${eventData?.eventTagline}-dialog`}
-            label="Event Tagline"
-            value={eventData?.eventTagline}
-            onChange={(e) => onChangeText(e, "eventTagline")}
-            disabled={loading}
-          />
-
-          <TextField
-            id={`${eventData?.shortDesc}-dialog`}
-            label="Short Description"
-            value={eventData?.shortDesc}
-            onChange={(e) => onChangeText(e, "shortDesc")}
-            disabled={loading}
-          />
-
-          <TextField
-            id={`${eventData?.longDesc}-dialog`}
-            label="Long Description"
-            value={eventData?.longDesc}
-            onChange={(e) => onChangeText(e, "longDesc")}
-            multiline
-            disabled={loading}
-          />
-
-          <TextField
-            id={`${eventData?.note}-dialog`}
-            label="Note"
-            value={eventData?.note}
-            onChange={(e) => onChangeText(e, "note")}
-            disabled={loading}
-          />
-
-          <TextField
-            id={`${eventData?.overview}-dialog`}
-            label="Overview"
-            value={eventData?.overview}
-            onChange={(e) => onChangeText(e, "overview")}
-            disabled={loading}
-          />
-
           <Grid2
             container
             rowSpacing={1}
             columnSpacing={{ xs: 1, sm: 2, md: 3 }}
           >
             <Grid2 size={6}>
-              <DatePicker
-                className="flex w-full"
-                id={`${event?.schedule?.registrationStart}-dialog`}
-                label="Registration Start"
-                value={dayjs(eventData?.schedule?.registrationStart)}
-                onChange={(e) => onChangeDate(e, "registrationStart")}
-                disabled={loading}
-              />
+              <FormControl fullWidth>
+                <InputLabel id={`${event?.status}-dialog`}>Status</InputLabel>
+                <Select
+                  label="Status"
+                  id={`${event?.status}-dialog-input`}
+                  value={eventData?.status}
+                  onChange={(e) => onChangeText(e, "status")}
+                  disabled={loading}
+                >
+                  <MenuItem value="upcoming">Upcoming</MenuItem>
+                  <MenuItem value="registrations">Registrations</MenuItem>
+                  <MenuItem value="ongoing">Ongoing</MenuItem>
+                  <MenuItem value="ended">Ended</MenuItem>
+                </Select>
+              </FormControl>
             </Grid2>
             <Grid2 size={6}>
-              <DatePicker
-                className="flex w-full"
-                id={`${event?.schedule?.registrationEnd}-dialog`}
-                label="Registration End"
-                value={dayjs(eventData?.schedule?.registrationEnd)}
-                onChange={(e) => onChangeDate(e, "registrationEnd")}
-                disabled={loading}
-              />
-            </Grid2>
-
-            <Grid2 size={6}>
-              <DatePicker
-                className="flex w-full"
-                id={`${event?.schedule?.eventStart}-dialog`}
-                label="Event Start"
-                value={dayjs(eventData?.schedule?.eventStart)}
-                onChange={(e) => onChangeDate(e, "eventStart")}
-                disabled={loading}
-              />
+              <FormControl fullWidth>
+                <InputLabel id={`${event?.eventType}-dialog`}>
+                  Event Type
+                </InputLabel>
+                <Select
+                  label="Event Type"
+                  id={`${event?.eventType}-dialog-input`}
+                  value={eventData?.eventType}
+                  onChange={(e) => onChangeText(e, "eventType")}
+                  disabled={loading}
+                >
+                  <MenuItem value="prefest">Prefest</MenuItem>
+                  <MenuItem value="onfest">Onfest</MenuItem>
+                </Select>
+              </FormControl>
             </Grid2>
             <Grid2 size={6}>
-              <DatePicker
-                className="flex w-full"
-                id={`${event?.schedule?.eventEnd}-dialog`}
-                label="Event End"
-                value={dayjs(eventData?.schedule?.eventEnd)}
-                onChange={(e) => onChangeDate(e, "eventEnd")}
-                disabled={loading}
-              />
+              <FormControl fullWidth>
+                <InputLabel id={`${event?.eventMode}-dialog`}>
+                  Event Type
+                </InputLabel>
+                <Select
+                  label="Event Mode"
+                  id={`${event?.eventMode}-dialog-input`}
+                  value={eventData?.eventMode}
+                  onChange={(e) => onChangeText(e, "eventMode")}
+                  disabled={loading}
+                >
+                  <MenuItem value="online">Online</MenuItem>
+                  <MenuItem value="offline">Offline</MenuItem>
+                  <MenuItem value="unstop">Unstop</MenuItem>
+                </Select>
+              </FormControl>
             </Grid2>
-
             <Grid2 size={6}>
-              <DatePicker
-                className="flex w-full"
-                id={`${event?.schedule?.submissionStart}-dialog`}
-                label="Submission Start"
-                value={dayjs(eventData?.schedule?.submissionStart)}
-                onChange={(e) => onChangeDate(e, "submissionStart")}
-                disabled={loading}
-              />
-            </Grid2>
-
-            <Grid2 size={6}>
-              <DatePicker
-                className="flex w-full"
-                id={`${event?.schedule?.submissionEnd}-dialog`}
-                label="Submission End"
-                value={dayjs(eventData?.schedule?.submissionEnd)}
-                onChange={(e) => onChangeDate(e, "submissionEnd")}
+              <TextField
+                fullWidth
+                id={`${eventData?.eventRedirectUrl}-dialog`}
+                label="Event Redirect URL"
+                value={eventData?.eventRedirectUrl}
+                onChange={(e) => onChangeText(e, "eventRedirectUrl")}
                 disabled={loading}
               />
             </Grid2>
           </Grid2>
+          <Grid2 size={6}>
+            <TextField
+              fullWidth
+              id={`${eventData?.eventParticipants}-dialog`}
+              label="Event Participants"
+              value={eventData?.eventParticipants}
+              onChange={(e) => onChangeText(e, "eventParticipants")}
+              disabled={loading}
+            />
+          </Grid2>
+          <Grid2 size={6}>
+            <TextField
+              fullWidth
+              id={`${eventData?.eventTagline}-dialog`}
+              label="Event Tagline"
+              value={eventData?.eventTagline}
+              onChange={(e) => onChangeText(e, "eventTagline")}
+              disabled={loading}
+            />
+          </Grid2>
+          <Grid2 size={6}>
+            <TextField
+              fullWidth
+              id={`${eventData?.shortDesc}-dialog`}
+              label="Short Description"
+              value={eventData?.shortDesc}
+              onChange={(e) => onChangeText(e, "shortDesc")}
+              disabled={loading}
+            />
+          </Grid2>
+          <Grid2 size={6}>
+            <TextField
+              fullWidth
+              id={`${eventData?.longDesc}-dialog`}
+              label="Long Description"
+              value={eventData?.longDesc}
+              onChange={(e) => onChangeText(e, "longDesc")}
+              multiline
+              disabled={loading}
+            />
+          </Grid2>
+          <Grid2 size={6}>
+            <TextField
+              fullWidth
+              id={`${eventData?.note}-dialog`}
+              label="Note"
+              value={eventData?.note}
+              onChange={(e) => onChangeText(e, "note")}
+              disabled={loading}
+            />
+          </Grid2>
+          <Grid2 size={6}>
+            <TextField
+              fullWidth
+              id={`${eventData?.overview}-dialog`}
+              label="Overview"
+              value={eventData?.overview}
+              onChange={(e) => onChangeText(e, "overview")}
+              disabled={loading}
+            />
+          </Grid2>
+          <Grid2 size={6}>
+            <DatePicker
+              className="flex w-full"
+              id={`${event?.schedule?.registrationStart}-dialog`}
+              label="Registration Start"
+              value={dayjs(eventData?.schedule?.registrationStart)}
+              onChange={(e) => onChangeDate(e, "registrationStart")}
+              disabled={loading}
+            />
+          </Grid2>
+          <Grid2 size={6}>
+            <DatePicker
+              className="flex w-full"
+              id={`${event?.schedule?.registrationEnd}-dialog`}
+              label="Registration End"
+              value={dayjs(eventData?.schedule?.registrationEnd)}
+              onChange={(e) => onChangeDate(e, "registrationEnd")}
+              disabled={loading}
+            />
+          </Grid2>
+          <Grid2 size={6}>
+            <DatePicker
+              className="flex w-full"
+              id={`${event?.schedule?.eventStart}-dialog`}
+              label="Event Start"
+              value={dayjs(eventData?.schedule?.eventStart)}
+              onChange={(e) => onChangeDate(e, "eventStart")}
+              disabled={loading}
+            />
+          </Grid2>
+          <Grid2 size={6}>
+            <DatePicker
+              className="flex w-full"
+              id={`${event?.schedule?.eventEnd}-dialog`}
+              label="Event End"
+              value={dayjs(eventData?.schedule?.eventEnd)}
+              onChange={(e) => onChangeDate(e, "eventEnd")}
+              disabled={loading}
+            />
+          </Grid2>
+          <Grid2 size={6}>
+            <DatePicker
+              className="flex w-full"
+              id={`${event?.schedule?.submissionStart}-dialog`}
+              label="Submission Start"
+              value={dayjs(eventData?.schedule?.submissionStart)}
+              onChange={(e) => onChangeDate(e, "submissionStart")}
+              disabled={loading}
+            />
+          </Grid2>
+          <Grid2 size={6}>
+            <DatePicker
+              className="flex w-full"
+              id={`${event?.schedule?.submissionEnd}-dialog`}
+              label="Submission End"
+              value={dayjs(eventData?.schedule?.submissionEnd)}
+              onChange={(e) => onChangeDate(e, "submissionEnd")}
+              disabled={loading}
+            />
+          </Grid2>
 
-          {/* Event Structure */}
           <Divider />
           <Grid2
             container
