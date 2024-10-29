1d10eafac6edd6023cfde5ca38a3e31a8e1b09da
Kaustav <kaustavkaran2015@gmail.com>
2024-10-30 00:54:04 +0530

    Refactor: Update PlanListingComponent to include view and delete actions
    - Added view and delete actions to the PlanListingComponent
    - Updated columns to include actionColumn with buttons for view and delete
    - Implemented handleView and handleDelete functions to handle button clicks
    - Added Dialog component to display selected row data when view button is clicked
    - Updated handleClose function to close the Dialog component

--- src/components/PlanListingComponent/PlanListingComponent.jsx
+++ src/components/PlanListingComponent/PlanListingComponent.jsx
@@ -1,16 +1,73 @@
-import { Paper } from "@mui/material";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Grid2,
+  Paper,
+  Typography,
+} from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
-import React from "react";
+import React, { useState } from "react";
+import PlanDialog from "./PlanDialog";
+import axios from "axios";
 
 export default function PlanListingComponent({ planRegistrationData }) {
-const columns = [
+  const [open, setOpen] = useState(false);
+  const [selectedRow, setSelectedRow] = useState({});
+
+  const columns = [
     { field: "referralCode", headerName: "Referral Code", flex: 1 },
     { field: "name", headerName: "Name", flex: 1 },
     { field: "varient", headerName: "Varient", flex: 1 },
     { field: "planType", headerName: "Plan Type", flex: 1 },
     { field: "price", headerName: "Price", flex: 1 },
     { field: "ttl", headerName: "TTL", flex: 1 },
-];
+  ];
+
+  const handleView = (row) => {
+    setSelectedRow(row);
+    setOpen(true);
+  };
+
+  const handleDelete = async (row) => {
+    try {
+      const response = await axios.delete(
+        `https://api.sinusoid.in/prplans/${row.referralCode}`
+      );
+      console.log("Delete Plan", response);
+      window.location.reload();
+    } catch (error) {
+      console.log("Error Deleting Plan", error);
+    }
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setSelectedRow(null);
+  };
+
+  const actionColumn = {
+    field: "actions",
+    headerName: "Actions",
+    width: 150,
+    renderCell: (params) => {
+      return (
+        <div>
+          <Button
+            onClick={() => handleView(params.row)}
+            // style={{ marginRight: "5px" }}
+          >
+            View
+          </Button>
+          <Button onClick={() => handleDelete(params.row)}>Delete</Button>
+        </div>
+      );
+    },
+  };
+
+  const updatedColumns = [...columns, actionColumn];
 
   const rows = planRegistrationData
     ? planRegistrationData.map((plan, index) => {
@@ -30,21 +87,137 @@ const columns = [
     page: 0,
     pageSize: 5,
   };
-return (
-    <div style={{ display: "flex", justifyContent: "center", marginBottom: "1rem" }}>
-        <Paper sx={{ height: 900, width: "80%" }}>
-            <DataGrid
-                id="plan-registration-listing"
-                rows={rows}
-                columns={columns}
-                pageSize={paginationModel.pageSize}
-                rowsPerPageOptions={[5, 10, 20]}
-                pagination
-                page={paginationModel.page}
-                rowCount={planRegistrationData?.length}
-                onCellClick={() => {}}
-            />
-        </Paper>
-    </div>
-);
+  return (
+    <>
+      <div>
+        <DataGrid
+          id="plan-registration-listing"
+          rows={rows}
+          columns={updatedColumns}
+          pageSize={paginationModel.pageSize}
+          rowsPerPageOptions={[5, 10, 20]}
+          pagination
+          page={paginationModel.page}
+          rowCount={planRegistrationData?.length}
+          onCellClick={() => {}}
+        />
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>View Data</DialogTitle>
+          <DialogContent>
+            {selectedRow && (
+              <Grid2
+                container
+                spacing={2}
+                className="p-4 bg-white rounded-lg shadow-md"
+              >
+                {/* <Grid2 item xs={12} className="mb-4">
+                  <Typography
+                    variant="h6"
+                    className="text-xl font-bold text-gray-800"
+                  >
+                    Plan Details
+                  </Typography>
+                </Grid2> */}
+                <Grid2 item xs={6} className="mb-2">
+                  <Typography
+                    variant="subtitle1"
+                    className="text-sm font-medium text-gray-600"
+                  >
+                    ID:
+                  </Typography>
+                </Grid2>
+                <Grid2 item xs={6} className="mb-2">
+                  <Typography variant="body1" className="text-sm text-gray-800">
+                    {selectedRow.id}
+                  </Typography>
+                </Grid2>
+                <Grid2 item xs={6} className="mb-2">
+                  <Typography
+                    variant="subtitle1"
+                    className="text-sm font-medium text-gray-600"
+                  >
+                    Referral Code:
+                  </Typography>
+                </Grid2>
+                <Grid2 item xs={6} className="mb-2">
+                  <Typography variant="body1" className="text-sm text-gray-800">
+                    {selectedRow.referralCode}
+                  </Typography>
+                </Grid2>
+                <Grid2 item xs={6} className="mb-2">
+                  <Typography
+                    variant="subtitle1"
+                    className="text-sm font-medium text-gray-600"
+                  >
+                    Name:
+                  </Typography>
+                </Grid2>
+                <Grid2 item xs={6} className="mb-2">
+                  <Typography variant="body1" className="text-sm text-gray-800">
+                    {selectedRow.name}
+                  </Typography>
+                </Grid2>
+                <Grid2 item xs={6} className="mb-2">
+                  <Typography
+                    variant="subtitle1"
+                    className="text-sm font-medium text-gray-600"
+                  >
+                    Varient:
+                  </Typography>
+                </Grid2>
+                <Grid2 item xs={6} className="mb-2">
+                  <Typography variant="body1" className="text-sm text-gray-800">
+                    {selectedRow.varient}
+                  </Typography>
+                </Grid2>
+                <Grid2 item xs={6} className="mb-2">
+                  <Typography
+                    variant="subtitle1"
+                    className="text-sm font-medium text-gray-600"
+                  >
+                    Plan Type:
+                  </Typography>
+                </Grid2>
+                <Grid2 item xs={6} className="mb-2">
+                  <Typography variant="body1" className="text-sm text-gray-800">
+                    {selectedRow.planType}
+                  </Typography>
+                </Grid2>
+                <Grid2 item xs={6} className="mb-2">
+                  <Typography
+                    variant="subtitle1"
+                    className="text-sm font-medium text-gray-600"
+                  >
+                    Price:
+                  </Typography>
+                </Grid2>
+                <Grid2 item xs={6} className="mb-2">
+                  <Typography variant="body1" className="text-sm text-gray-800">
+                    {selectedRow.price}
+                  </Typography>
+                </Grid2>
+                <Grid2 item xs={6} className="mb-2">
+                  <Typography
+                    variant="subtitle1"
+                    className="text-sm font-medium text-gray-600"
+                  >
+                    TTL:
+                  </Typography>
+                </Grid2>
+                <Grid2 item xs={6} className="mb-2">
+                  <Typography variant="body1" className="text-sm text-gray-800">
+                    {selectedRow.ttl}
+                  </Typography>
+                </Grid2>
+              </Grid2>
+            )}
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Close</Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+      {/* <PlanDialog loadPlanData={} onClose={} open={} /> */}
+    </>
+  );
 }
--- src/components/PlanRegistrationComponents/PlanRegistrationListingComponent.jsx
+++ src/components/PlanRegistrationComponents/PlanRegistrationListingComponent.jsx
@@ -1,11 +1,23 @@
-import { Paper, Dialog, DialogContent, DialogTitle } from "@mui/material";
+import {
+  Paper,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Grid2,
+  Typography,
+  DialogActions,
+  Button,
+} from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { render } from "@testing-library/react";
+import axios from "axios";
 import React, { useState } from "react";
 
 export default function PlanRegistrationListingComponent({
   planRegistrationData,
 }) {
+  const [open, setOpen] = useState(false);
+  const [selectedRow, setSelectedRow] = useState({});
   const [openPhotoId, setOpenPhotoId] = useState(false);
   const [openPaymentProof, setOpenPaymentProof] = useState(false);
   const [selectedPhotoIdUrl, setSelectedPhotoIdUrl] = useState("");
@@ -34,6 +46,47 @@ export default function PlanRegistrationListingComponent({
   const handleClosePaymentProofUrl = ({ par }) => {
     setOpenPhotoId(false);
     setSelectedPaymentProofUrl("");
+  };
+
+  const handleView = (row) => {
+    setSelectedRow(row);
+    setOpen(true);
+  };
+
+  const handleDelete = async (row) => {
+    try {
+      const response = await axios.delete(
+        `https://api.sinusoid.in/plan/${row.registrationId}`
+      );
+      console.log("Delete Plan", response);
+      window.location.reload();
+    } catch (error) {
+      console.log("Error Deleting Plan", error);
+    }
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setSelectedRow(null);
+  };
+
+  const actionColumn = {
+    field: "actions",
+    headerName: "Actions",
+    width: 150,
+    renderCell: (params) => {
+      return (
+        <div>
+          <Button
+            onClick={() => handleView(params.row)}
+            // style={{ marginRight: "5px" }}
+          >
+            View
+          </Button>
+          <Button onClick={() => handleDelete(params.row)}>Delete</Button>
+        </div>
+      );
+    },
   };
 
   const columns = [
@@ -82,21 +135,137 @@ export default function PlanRegistrationListingComponent({
     : [];
   const paginationModel = { page: 0, pageSize: 5 };
 
+  const updatedColumns = [...columns, actionColumn];
+
   return (
     <>
-      <Paper sx={{ height: 900, width: "100%" }}>
-        <DataGrid
-          id="plan-registration-listing"
-          rows={rows}
-          columns={columns}
-          pageSize={paginationModel.pageSize}
-          rowsPerPageOptions={[5, 10, 20]}
-          pagination
-          page={paginationModel.page}
-          rowCount={planRegistrationData?.length}
-          onCellClick={handleCellClick}
-        />
-      </Paper>
+      <div>
+        <Paper sx={{ height: 900, width: "100%" }}>
+          <DataGrid
+            id="plan-registration-listing"
+            rows={rows}
+            columns={updatedColumns}
+            pageSize={paginationModel.pageSize}
+            rowsPerPageOptions={[5, 10, 20]}
+            pagination
+            page={paginationModel.page}
+            rowCount={planRegistrationData?.length}
+            onCellClick={() => {}}
+          />
+        </Paper>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>View Data</DialogTitle>
+          <DialogContent>
+            <DialogContent>
+              {selectedRow && (
+                <Grid2
+                  container
+                  spacing={2}
+                  className="p-4 bg-white rounded-lg shadow-md"
+                >
+                  {/* <Grid2 item xs={12} className="mb-4">
+                    <Typography
+                      variant="h6"
+                      className="text-xl font-bold text-gray-800"
+                    >
+                      Plan Details
+                    </Typography>
+                  </Grid2> */}
+                  <Grid2 item xs={6} className="mb-2">
+                    <Typography
+                      variant="subtitle1"
+                      className="text-sm font-medium text-gray-600"
+                    >
+                      Registration ID:
+                    </Typography>
+                  </Grid2>
+                  <Grid2 item xs={6} className="mb-2">
+                    <Typography
+                      variant="body1"
+                      className="text-sm text-gray-800"
+                    >
+                      {selectedRow.registrationId}
+                    </Typography>
+                  </Grid2>
+                  <Grid2 item xs={6} className="mb-2">
+                    <Typography
+                      variant="subtitle1"
+                      className="text-sm font-medium text-gray-600"
+                    >
+                      Plan Name:
+                    </Typography>
+                  </Grid2>
+                  <Grid2 item xs={6} className="mb-2">
+                    <Typography
+                      variant="body1"
+                      className="text-sm text-gray-800"
+                    >
+                      {selectedRow.planName}
+                    </Typography>
+                  </Grid2>
+                  <Grid2 item xs={6} className="mb-2">
+                    <Typography
+                      variant="subtitle1"
+                      className="text-sm font-medium text-gray-600"
+                    >
+                      Name: {selectedRow.firstName} {selectedRow.lastName}
+                    </Typography>
+                  </Grid2>
+                  <Grid2 item xs={12} className="mb-2">
+                    <Typography
+                      variant="subtitle1"
+                      className="text-sm font-medium text-gray-600"
+                    >
+                      Phone:
+                    </Typography>
+                  </Grid2>
+                  <Grid2 item xs={6} className="mb-2">
+                    <Typography
+                      variant="body1"
+                      className="text-sm text-gray-800"
+                    >
+                      {selectedRow.phone}
+                    </Typography>
+                  </Grid2>
+                  <Grid2 item xs={6} className="mb-2">
+                    <Typography
+                      variant="subtitle1"
+                      className="text-sm font-medium text-gray-600"
+                    >
+                      Photo ID:
+                    </Typography>
+                  </Grid2>
+                  <Grid2 item xs={6} className="mb-2">
+                    <img
+                      src={`https://api.sinusoid.in/images/${selectedRow.photoIdUrl}`}
+                      alt="ID Proof"
+                      className="w-full rounded-lg shadow-md"
+                    />
+                  </Grid2>
+                  <Grid2 item xs={6} className="mb-2">
+                    <Typography
+                      variant="subtitle1"
+                      className="text-sm font-medium text-gray-600"
+                    >
+                      Payment Proof:
+                    </Typography>
+                  </Grid2>
+                  <Grid2 item xs={6} className="mb-2">
+                    <img
+                      src={`https://api.sinusoid.in/images/${selectedRow.paymentProofUrl}`}
+                      alt="Payment Proof"
+                      className="w-full rounded-lg shadow-md"
+                    />
+                  </Grid2>
+                </Grid2>
+              )}
+            </DialogContent>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Close</Button>
+          </DialogActions>
+        </Dialog>
+      </div>
       <Dialog open={openPhotoId} onClose={handleClosePhotoIdUrl}>
         <DialogTitle>Photo ID</DialogTitle>
         <DialogContent>
