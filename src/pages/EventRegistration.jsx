e81bceb6e1c503e55fdf6423c76b185842e67fdd
Kaustav <kaustavkaran2015@gmail.com>
2024-11-03 10:42:08 +0530

    Feat: Add buttons to check and update Google Sheet in EventRegistration component

--- src/pages/EventRegistration.jsx
+++ src/pages/EventRegistration.jsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from "react";
-import ProtectedHeader from "../components/Header";
+import { Link, Upload } from "@mui/icons-material";
+import { Button } from "@mui/material";
 import axios from "axios";
+import React, { useEffect, useState } from "react";
 import EventRegistrationLisitngComponent from "../components/EventRegistrationComponents/EventRegistrationLisitngComponent";
+import ProtectedHeader from "../components/Header";
 
 export default function EventRegistration() {
   const [eventRegistrationData, setEventRegistrationData] = useState(null);
@@ -16,6 +18,19 @@ export default function EventRegistration() {
       return response?.data;
     } catch (error) {
       console.error("Error fetching event registrations:", error);
+      return null;
+    }
+  };
+
+  const updateGsheet = async () => {
+    try {
+      const response = await axios.post(
+        "https://api.sinusoid.in/eventRegistrationGsheet"
+      );
+
+      return response?.data;
+    } catch (error) {
+      console.error("Error updating G-Sheet:", error);
       return null;
     }
   };
@@ -35,7 +50,24 @@ export default function EventRegistration() {
   return (
     <>
       <ProtectedHeader />
-      <div className="flex mt-20 mx-auto flex-col w-fit">
+      <div className="flex mt-20 mx-auto flex-col items-center justify-center w-fit">
+        <div className="flex justify-end w-full mb-3 gap-2">
+          <Button
+            variant="contained"
+            startIcon={<Link />}
+            href="https://docs.google.com/spreadsheets/d/1JNvdE69FYBWXa3Us_nLKShz1XSK2vCjvQciFofKtYP0/edit?usp=sharing"
+            target="_blank"
+          >
+            Check the G-Sheet
+          </Button>
+          <Button
+            variant="outlined"
+            startIcon={<Upload />}
+            onClick={updateGsheet}
+          >
+            Update the G-Sheet
+          </Button>
+        </div>
         <EventRegistrationLisitngComponent
           eventRegistrationData={eventRegistrationData}
         />
