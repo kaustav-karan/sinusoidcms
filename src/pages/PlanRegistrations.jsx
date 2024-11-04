6cd8c179bbb81e9da269e352d9226c49ba70c019
Kaustav <kaustavkaran2015@gmail.com>
2024-11-04 11:46:19 +0530

    Feat: Add buttons to check and update Google Sheet in PlanRegistrations component

--- src/pages/PlanRegistrations.jsx
+++ src/pages/PlanRegistrations.jsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import ProtectedHeader from "../components/Header";
 import PlanRegistrationListingComponent from "../components/PlanRegistrationComponents/PlanRegistrationListingComponent";
+import { Button } from "@mui/material";
+import { Link, Upload } from "@mui/icons-material";
 
 export default function PlanRegistrations() {
   const [planRegistrationData, setPlanRegistrationData] = useState(null);
@@ -18,6 +20,19 @@ export default function PlanRegistrations() {
     }
   };
 
+  const updateGsheet = async () => {
+    try {
+      const response = await axios.post(
+        "https://api.sinusoid.in/planRegistrationGsheet"
+      );
+
+      return response?.data;
+    } catch (error) {
+      console.error("Error updating G-Sheet:", error);
+      return null;
+    }
+  };
+
   useEffect(() => {
     const fetchData = async () => {
       const data = await FetchPlanRegistrations();
@@ -29,7 +44,24 @@ export default function PlanRegistrations() {
   return (
     <>
       <ProtectedHeader />
-      <div className="flex mt-20 mx-auto flex-col w-fit">
+      <div className="flex mt-20 mx-auto flex-col items-center justify-center w-fit">
+        <div className="flex justify-end w-full mb-3 gap-2">
+          <Button
+            variant="contained"
+            startIcon={<Link />}
+            href="https://docs.google.com/spreadsheets/d/1_6_ncEp9SCD_J_xQIclQXU8sX8IP-czaP78voGFn3FI/edit?usp=sharing"
+            target="_blank"
+          >
+            Check the G-Sheet
+          </Button>
+          <Button
+            variant="outlined"
+            startIcon={<Upload />}
+            onClick={updateGsheet}
+          >
+            Update the G-Sheet
+          </Button>
+        </div>
         <PlanRegistrationListingComponent
           planRegistrationData={planRegistrationData}
         />
